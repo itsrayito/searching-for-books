9bb8093d1fb28ee7cbe30ae700201fd850b76585
itsrayito <rayito.aguirre94@gmail.com>
2023-07-24 03:15:39 -0500

    configuring connection for mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks' , {
+    useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
